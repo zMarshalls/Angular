52cb351cb4ebd8f4ea08bf2ff5f5c6a962dbbcc5
zMarshalls <zMarshalls>
2018-08-31 20:09:57 -0300

    Incluindo elementos no form do cadastro, e inclusão do jquery para mascaras.

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -11,7 +11,17 @@ export class SignupComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Inicializa os elementos
     $('select').formSelect();
+    $('.datepicker').datepicker();
+
+    // Inicializa as mascaras
+    $('#cpf').mask('000.000.000-00');
+    $('#fone').mask('(00) 0 0000-0000');
+    $('#cep').mask('00000-000');
+
+
   }
+
 
 }
